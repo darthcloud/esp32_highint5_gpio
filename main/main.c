59f9cf80c775f6c9de6b8e540f7a138e8694462d
Jacques Gagnon <darthcloud@gmail.com>
2019-08-21 10:13:44 -0400

    Cleanup example

--- main/main.c
+++ main/main.c
@@ -6,12 +6,18 @@
 #include "esp_intr_alloc.h"
 #include "driver/gpio.h"
 
-uint32_t test;
+/* See "interrupt cpu using table" in esp-idf/components/soc/esp32/include/soc/soc.h */
+/* We could use 26 or 31 for an level 5 interrupt */
+#define INTR_NUM 31
+#define CPU_CORE 1
+#define GPIO_NUM 26
+
+uint32_t intr_cnt = 0;
 
 static void cpu1_task(void *arg) {
     gpio_config_t io_conf = {
         .intr_type = GPIO_PIN_INTR_NEGEDGE,
-        .pin_bit_mask = (1ULL << 26),
+        .pin_bit_mask = (1ULL << GPIO_NUM),
         .mode = GPIO_MODE_INPUT,
         .pull_down_en = GPIO_PULLDOWN_DISABLE,
         .pull_up_en = GPIO_PULLUP_DISABLE
@@ -20,15 +26,16 @@ static void cpu1_task(void *arg) {
     gpio_config(&io_conf);
 
 #if 1
-    ESP_INTR_DISABLE(26);
-    intr_matrix_set(1, ETS_GPIO_INTR_SOURCE, 26);
-    ESP_INTR_ENABLE(26);
+    ESP_INTR_DISABLE(INTR_NUM);
+    intr_matrix_set(CPU_CORE, ETS_GPIO_INTR_SOURCE, INTR_NUM);
+    ESP_INTR_ENABLE(INTR_NUM);
 #else
+    /* Should be the proper way, but doesn't work  */
     esp_intr_alloc(ETS_UART1_INTR_SOURCE, ESP_INTR_FLAG_LEVEL5 | ESP_INTR_FLAG_IRAM, NULL, NULL, NULL);
 #endif
 
     while (1) {
-        printf("%s alive test: %u\n", __FUNCTION__, test);
+        printf("%s alive intr_cnt: %u\n", __FUNCTION__, intr_cnt);
         vTaskDelay(1000 / portTICK_PERIOD_MS);
     }
 }
